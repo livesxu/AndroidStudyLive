8f11be361b5a58ef370f5d696ab7e72908a2fc91
livesxu <livesxu@163.com>
2019-05-29 20:24:54 +0800

    基础UI

--- MyApplication0/app/src/main/java/com/example/myapplication0/MainActivity.java
+++ MyApplication0/app/src/main/java/com/example/myapplication0/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.myapplication0;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nomal_some);
+
     }
 }
