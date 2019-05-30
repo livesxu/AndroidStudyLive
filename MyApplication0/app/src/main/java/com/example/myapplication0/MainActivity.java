8cc9b6966d207cf1e9124b0e8657f04c701d68bd
livesxu <livesxu@163.com>
2019-05-30 10:26:36 +0800

    时间选择器

--- MyApplication0/app/src/main/java/com/example/myapplication0/MainActivity.java
+++ MyApplication0/app/src/main/java/com/example/myapplication0/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication0;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nomal_some);
+
+        DatePicker picker = findViewById(R.id.datePicker);
+
+        //月份起始为0
+        picker.init(1992, 11, 21, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                Toast.makeText(getApplicationContext(),year + "年"+(monthOfYear + 1)+"月"+dayOfMonth + "日",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        TimePicker timerPicker = findViewById(R.id.timerPicker);
+
+        timerPicker.setIs24HourView(true);
+
+        timerPicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+
+                Toast.makeText(getApplicationContext(),hourOfDay + "h" + minute + "min",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
