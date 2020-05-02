08678600b51413e4835d6578ec6de8dea5b83eae
Livesxu <livesxu@163.com>
2020-05-02 14:35:19 +0800

    回传值

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.myapplication2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.LogPrinter;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +24,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 class Person implements Serializable {
     String name;
@@ -71,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
                 new int[]{android.R.id.text1}
         );
         listView.setAdapter(adapter);
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -93,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("person",person);
 //                intent.setClass(MainActivity.this,MyActivity.class);
                 intent.setAction("haha");
-                startActivity(intent);
+                startActivityForResult(intent,0);
             }
         });
         listView.setOnScrollListener(new AbsListView.OnScrollListener() {
@@ -107,6 +110,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String string = data.getStringExtra("back");
+
+        Log.v("zzz",string);
     }
 
     @Override
--- MyApplication2/app/src/main/java/com/example/myapplication2/MyActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MyActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MyActivity extends AppCompatActivity {
@@ -16,7 +17,17 @@ public class MyActivity extends AppCompatActivity {
 //        String title = intent.getStringExtra("title");
         Person person = (Person) intent.getSerializableExtra("person");
 
-        TextView textView = findViewById(R.id.textView);
+        final TextView textView = findViewById(R.id.textView);
         textView.setText(person.name);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent();
+                intent1.putExtra("back",textView.getText());
+                setResult(0,intent1);
+                finish();
+            }
+        });
     }
 }
