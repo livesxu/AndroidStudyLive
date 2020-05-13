b88ec686aeabee5ca2edaefe382c159854e9c566
Livesxu <livesxu@163.com>
2020-05-13 21:13:59 +0800

    fragment传值

--- MyApplication3/app/src/main/java/com/example/myapplication3/Fragment1.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/Fragment1.java
@@ -1,9 +1,15 @@
 package com.example.myapplication3;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +17,47 @@ import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment {
 
+    private String par;
+    private Activity mActivity;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (Activity)context;
+        par = getArguments().getString("key");
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment1,container,false);
+        TextView textView = view.findViewById(R.id.text1);
+        textView.setText(par);
+
+        Button button = view.findViewById(R.id.button1);
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Log.v("zzz","f1里面的点击");
+
+                ((FragmentActivity)mActivity).f1ButtonAction();
+                return true;
+            }
+        });
 
         return view;
     }
+
+    public interface CallBack{
+
+        public void getResult(String string);
+    }
+
+    public void onData(CallBack callBack){
+        Button button = getView().findViewById(R.id.button1);
+        Log.v("zzz",button.getText().toString());
+        callBack.getResult("回传的某个值");
+    }
+
 }
--- MyApplication3/app/src/main/java/com/example/myapplication3/FragmentActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/FragmentActivity.java
@@ -1,17 +1,13 @@
 package com.example.myapplication3;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 public class FragmentActivity extends AppCompatActivity {
 
@@ -21,47 +17,63 @@ public class FragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        textViews = new ArrayList<>();
-        for (int i = 0 ;i< 10;i++){
-
-            TextView textView = new TextView(getApplicationContext());
-            textView.setTextColor(Color.RED);
-            textView.setText(String.format("第%d个页面",i));
-            textViews.add(textView);
-        }
-
-        ViewPager viewPager = findViewById(R.id.viewpager1);
-        viewPager.setAdapter(new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return textViews.size();
-            }
-
-            @Override
-            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-                return view == object;
-            }
-
-            @NonNull
-            @Override
-            public Object instantiateItem(@NonNull ViewGroup container, int position) {
-
-                TextView view = textViews.get(position);
-                container.addView(view);
-
-                return view;
-            }
-
-            @Override
-            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-
-                container.removeView((View)object);
-            }
-        });
+//        textViews = new ArrayList<>();
+//        for (int i = 0 ;i< 10;i++){
+//
+//            TextView textView = new TextView(getApplicationContext());
+//            textView.setTextColor(Color.RED);
+//            textView.setText(String.format("第%d个页面",i));
+//            textViews.add(textView);
+//        }
+//
+//        ViewPager viewPager = findViewById(R.id.viewpager1);
+//        viewPager.setAdapter(new PagerAdapter() {
+//            @Override
+//            public int getCount() {
+//                return textViews.size();
+//            }
+//
+//            @Override
+//            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+//                return view == object;
+//            }
+//
+//            @NonNull
+//            @Override
+//            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+//
+//                TextView view = textViews.get(position);
+//                container.addView(view);
+//
+//                return view;
+//            }
+//
+//            @Override
+//            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+//
+//                container.removeView((View)object);
+//            }
+//        });
 
 
         Fragment1 fragment1 = new Fragment1();
+        Bundle bundle = new Bundle();
+        bundle.putString("key","传输的文本内容展示");
+        fragment1.setArguments(bundle);
+
         getSupportFragmentManager().beginTransaction().add(R.id.fragemt_test,fragment1,"f1").commit();
+    }
+
+    public void f1ButtonAction() {
+
+        Fragment1 fragment1 = (Fragment1) getSupportFragmentManager().findFragmentByTag("f1");
+        fragment1.onData(new Fragment1.CallBack() {
+            @Override
+            public void getResult(String string) {
+
+                Log.v("zzz",string);
+            }
+        });
     }
 
     public void clickAction(View view) {
